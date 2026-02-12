43c9e221f030a0a9486d55bf688e4108a1a364e8
Marlon Mosquera Rivera <marlon.mosquerar@gmail.com>
2026-02-12 11:51:51 -0500

    1. se realizo la configuracion inicial del proyecto se crearon las carpetas principales domain,application,infrastructure,web. y es hizo la configuracion inicial del applilcation.properties.

---
+++ src/main/java/gestion/de/tareas/gestor/tareas/GestorTareasApplication.java
@@ -0,0 +1,13 @@
+package gestion.de.tareas.gestor.tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestion/de/tareas/gestor/tareas/GestorTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package gestion.de.tareas.gestor.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
