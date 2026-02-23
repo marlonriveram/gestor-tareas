c2f0be4b416d64b88063c38f1442757993b04e57
Marlon Mosquera Rivera <marlon.mosquerar@gmail.com>
2026-02-22 20:21:09 -0500

    se removio el swagger

--- src/main/java/gestion/de/tareas/gestor/tareas/infrastructure/config/SecurityConfig.java
+++ src/main/java/gestion/de/tareas/gestor/tareas/infrastructure/config/SecurityConfig.java
@@ -33,10 +33,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/auth/*",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui.html"
+                                "/auth/*"
                         ).permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
